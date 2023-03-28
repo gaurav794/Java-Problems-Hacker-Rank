1a07b9bb3952f5d5420888e145c3432093e62ee1
gaurav794 <gauravsharma102000@gmail.com>
2023-03-28 18:20:27 +0530

    Test

---
+++ 1234.java
@@ -0,0 +1,1 @@
+echo "Adding files locally"
--- automate.sh
+++ automate.sh
@@ -1,13 +1,41 @@
 #Display message
 
 
-echo "Hello"
+echo "GITHUB JAVA-PROBLEMS-HACKER-RANK"
+echo
 
 
+#Read File name and content to generate file
+createFileWithContent()
+{
+  read -p 'File Name: ' fileName
+  vim $fileName.java
+}
+
+gitCommandsToPush()
+{
+echo
+git status
+echo
+echo "Adding files locally"
+echo
 git add .
+echo
+read -p 'Enter Commit Message: ' commitMsg
+echo
+git commit -m $commitMsg
+echo
+git status
+echo
 
-git commit -m "Testing"
+}
 
-git push origin main
+#Functions
+#Read File name and content to generate file
+createFileWithContent
+#Git commands to push the local changes to cloud on github
+gitCommandsToPush
 
-echo "DONE"
+
+
+
