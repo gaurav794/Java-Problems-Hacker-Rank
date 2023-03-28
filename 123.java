cafb3393b0107d1f3d6dc004522df4a03b5387a0
gaurav794 <gauravsharma102000@gmail.com>
2023-03-28 18:51:21 +0530

    automate.sh created

--- 123.java
+++
@@ -1,2 +0,0 @@
-echo "Adding files locally"
-echo
--- automate.sh
+++ automate.sh
@@ -1,14 +1,13 @@
 #Display message
-
-
-echo "GITHUB JAVA-PROBLEMS-HACKER-RANK"
 echo
-
+echo
+echo "====================GITHUB JAVA-PROBLEMS-HACKER-RANK===================="
+echo
 
 #Read File name and content to generate file
 createFileWithContent()
 {
-  read -p 'File Name: ' fileName
+  read -p 'ENTER FILE NAME: ' fileName
   vim $fileName.java
 }
 
@@ -18,23 +17,34 @@ echo
 git status
 echo
 git add .
-echo "FILES ADDED SUCCESSFULLY"
+echo "====================FILES ADDED SUCCESSFULLY===================="
 echo
 git status
 echo
-read -p 'Enter Commit Message: ' commitMsg
+read -p 'ENTER COMMIT MESSAGE: ' commitMsg
 echo
 git commit -m $commitMsg
 echo
 git status
 echo
+git push origin main
+echo
+echo "====================FILES PUSHED SUCCESSFULLY===================="
 }
 
-#Functions
-#Read File name and content to generate file
-createFileWithContent
-#Git commands to push the local changes to cloud on github
-gitCommandsToPush
+
+flag=1
+#Loop to iterate until user enters "q" else continue to execute
+while [ $flag -ne 0 ]
+do
+ #Functions
+ #Read File name and content to generate file
+ createFileWithContent
+ #Git commands to push the local changes to cloud on github
+ gitCommandsToPush
+ read -p 'ENTER "1" TO CONTINUE OR "0" TO EXIT: ' $flag
+done
+
 
 
 
