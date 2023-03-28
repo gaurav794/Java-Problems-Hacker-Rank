cb56166b6d029be008929703da5dc083706ea56e
gaurav794 <gauravsharma102000@gmail.com>
2023-03-28 19:14:21 +0530

    Welcome To Java!

---
+++ WelcomeToJava.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+/*
+Author: Gaurav Sharma
+*/
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+       System.out.println("Hello, World.");
+       System.out.println("Hello, Java."); 
+    }
+}
